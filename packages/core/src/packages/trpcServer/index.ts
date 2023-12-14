a62883c2622648e6b21df879cb7643898c9e9da8
Roshan K Gujarathi <roshankumargujarathi@gmail.com>
2023-12-14 15:42:20 +0530

    TRPC Type generator

--- packages/core/src/packages/trpcServer/index.ts
+++ packages/core/src/packages/trpcServer/index.ts
@@ -1,6 +1,8 @@
-import { Mercury, DB } from '../../mercury';
+import { Mercury } from '../../mercury';
 import { initTRPC, ProcedureRouterRecord } from '@trpc/server';
 import { z } from 'zod';
+import path from 'path';
+import fs from 'fs';
 
 const trpc = initTRPC.create();
 
@@ -14,9 +16,19 @@ declare module '../../mercury' {
       t: typeof trpc;
     };
   }
+
+  interface DB {
+    User: {
+      name: string;
+    };
+    Account: {
+      name: string;
+      user: string;
+    };
+  }
 }
 
-export default <Z extends string>(config?: MercuryServerOptions) => {
+export default (config?: MercuryServerOptions) => {
   return (mercury: Mercury) => {
     if (!config) {
       config = {
@@ -44,71 +56,87 @@ export default <Z extends string>(config?: MercuryServerOptions) => {
 
     mercury.trpc = {
       t: config.t,
-      appRouter: createTrpcServer<Z>(mercury, config),
+      appRouter: createTrpcServer(mercury, config),
     };
   };
 };
 
-function createTrpcServer<RModel extends string>(
-  mercury: Mercury,
-  options: MercuryServerOptions
-) {
+export const createInterfaces = (mercury: Mercury) => {
+  const rootDirectory = process.cwd();
+  const fileName = 'interfaces.ts';
+  const filePath = path.join(rootDirectory, fileName);
+
+  const recordType = mercury.list.map((model) => {
+    return `${model.name}: import("@trpc/server").BuildProcedure<
+  "query",
+  {
+    _config: import("@trpc/server").RootConfig<{
+      ctx: object;
+      meta: object;
+      errorShape: import("@trpc/server").DefaultErrorShape;
+      transformer: import("@trpc/server").DefaultDataTransformer;
+    }>;
+    _meta: object;
+    _ctx_out: object;
+    _input_in: {
+      ${Object.keys(model.fields)
+        .map((key) => {
+          return `${key}: string`;
+        })
+        .join(';\n')}
+    };
+    _input_out: {
+      id: string;
+    };
+    _output_in: typeof import("@trpc/server").unsetMarker;
+    _output_out: typeof import("@trpc/server").unsetMarker;
+  },
+  string
+>`;
+  });
+  const fileContent = `declare function createTrpcServer(): import("@trpc/server").CreateRouterInner<
+  import("@trpc/server").RootConfig<{
+    ctx: object;
+    meta: object;
+    errorShape: import("@trpc/server").DefaultErrorShape;
+    transformer: import("@trpc/server").DefaultDataTransformer;
+  }>,
+  {
+    ${recordType.join(';\n')}
+  }
+>;
+
+export type AppRouter = ReturnType<typeof createTrpcServer>;
+`;
+  fs.writeFileSync(filePath, fileContent, 'utf-8');
+};
+
+type DB = 'User' | 'Account';
+
+function createTrpcServer(mercury: Mercury, options: MercuryServerOptions) {
   const { t } = options;
   const procedure = t.procedure;
-  const genProcedure = (model: RModel) =>
-    procedure
-      .input(
-        z.object({
-          name: z.string(),
-        })
-      )
+  const genProcedure = (model: keyof DB) => {
+    const obj: any = {};
+    const modelFields = mercury.list.find((v) => v.name === model)?.fields;
+    if (modelFields) {
+      Object.entries(modelFields).forEach(([key, value]) => {
+        obj[key] = value.type == 'string' ? z.string().optional() : z.any();
+      });
+    }
+    return procedure
+      .input(z.object(obj))
       .query(
         ({ input }) => `Hello ${model.toString()}, you have sent ${input}!`
       );
-  const data: Record<RModel, ReturnType<typeof genProcedure>> = {} as Record<
-    RModel,
+  };
+  const data: Record<keyof DB, ReturnType<typeof genProcedure>> = {} as Record<
+    keyof DB,
     ReturnType<typeof genProcedure>
   >;
-  (Object.keys(mercury.db) as RModel[]).forEach((model: RModel) => {
+  (Object.keys(mercury.db) as (keyof DB)[]).forEach((model: keyof DB) => {
     data[model] = genProcedure(model);
   });
+  createInterfaces(mercury); // This will generate the type defs file
   return t.router(data);
-  //   const router = t.router(
-  //     model.reduce(
-  //       (a, v) => ({
-  //         ...a,
-  //         [v]: procedure
-  //           .input(
-  //             z.object({
-  //               name: z.string(),
-  //             })
-  //           )
-  //           .query(({ input }) => `Hello ${v}, you have sent ${input.name}!`),
-  //       }),
-  //       {}
-  //     )
-  //   );
-  //   const router = t.router({
-  //     greeting: procedure
-  //       // This is the input schema of your procedure
-  //       // 💡 Tip: Try changing this and see type errors on the client straight away
-  //       .input(
-  //         z.object({
-  //           name: z.string().nullish(),
-  //         })
-  //       )
-  //       .query(({ input }) => {
-  //         // This is what you're returning to your client
-  //         return {
-  //           text: `hello ${input?.name ?? 'world'}`,
-  //           // 💡 Tip: Try adding a new property here and see it propagate to the client straight-away
-  //         };
-  //       }),
-  //     // 💡 Tip: Try adding a new procedure here and see if you can use it in the client!
-  //     // getUser: publicProcedure.query(() => {
-  //     //   return { id: '1', name: 'bob' };
-  //     // }),
-  //   });
-
-  //   return router;
 }
