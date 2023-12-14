a34ec89d0040d5e5cc931924114d866e0e93a3f0
PrashanthBeri <prashanthberi00@gmail.com>
2023-12-14 17:59:04 +0530

    added zfieldmapping

--- packages/core/src/packages/trpcServer/index.ts
+++ packages/core/src/packages/trpcServer/index.ts
@@ -51,6 +51,40 @@ export default (config?: MercuryServerOptions) => {
   };
 };
 
+const zFieldMapping: { [type: string]: any } = {
+  string: z.string(),
+  number: z.number(),
+  float: z.number(),
+  boolean: z.boolean(),
+  date: z.date(),
+  relationship: z.string(),
+  virtual: z.string(),
+  mixed:z.string(),
+  objectId:z.string(),
+  decimal:z.number(),
+  undefined: z.any()
+}
+
+const getZField = (value: TField) => {
+  // @ts-ignore
+  if(value.type == 'enum') return zFieldMapping[value?.enumType];
+  let zfield = zFieldMapping[value.type];
+  if(value?.many) zfield = zfield.array();
+  if(!value?.required) zfield = zfield.optional();
+  return zfield;
+}
+
+const getZObj = (mercury: Mercury, model: keyof DB) => {
+  const obj: any = {};
+  const modelFields = mercury.list.find((v) => v.name === model)?.fields;
+  if (modelFields) {
+    Object.entries(modelFields).forEach(([key, value]) => {
+      obj[key] = getZField(value);
+    });
+  }
+  return obj;
+}
+
 export const createInterfaces = (mercury: Mercury) => {
   const rootDirectory = process.cwd();
   const fileName = 'interfaces.ts';
@@ -115,25 +149,51 @@ export type AppRouter = ReturnType<typeof createTrpcServer>;
 function createTrpcServer(mercury: Mercury, options: MercuryServerOptions) {
   const { t } = options;
   const procedure = t.procedure;
-  const genProcedure = (model: keyof DB) => {
-    const obj: any = {};
-    const modelFields = mercury.list.find((v) => v.name === model)?.fields;
-    if (modelFields) {
-      Object.entries(modelFields).forEach(([key, value]) => {
-        obj[key] = value.type == 'string' ? z.string().optional() : z.any();
-      });
-    }
+  const genProcedure = (model: keyof DB, action: string) => {
+    const obj: any = getZObj(mercury, model);
     return procedure
       .input(z.object(obj))
       .query(
-        async ({ input }) =>
-          await mercury.db[model].get(input, { id: '1', profile: 'User' })
+        async ({ input }) =>{
+          if(action === 'get'){
+            return await mercury.db[model].get(input, { id: '1', profile: 'User' })
+          } else if(action === 'list') {
+            return await mercury.db[model].list(input, { id: '1', profile: 'User' })
+          } else if(action === 'paginate') {
+            return await mercury.db[model].paginate(input.query, input.filters, { id: '1', profile: 'User' })
+          } else if(action === 'count') {
+            return await mercury.db[model].count( { id: '1', profile: 'User' })
+          } 
+        }
       );
   };
+
+  const genMutationProcedure = (model: keyof DB, action: string) => {
+    const obj: any = getZObj(mercury, model);
+    return procedure
+      .input(z.object(obj))
+      .mutation(
+        async ({ input }) =>{
+           if(action === 'create') {
+            return await mercury.db[model].create(input, { id: '1', profile: 'User' })
+          } else if(action === 'update') {
+            return await mercury.db[model].update(input.id, input, { id: '1', profile: 'User' })
+          } else if(action === 'delete') {
+            return await mercury.db[model].delete(input.id, { id: '1', profile: 'User' })
+          }
+        }
+      );
+  }
   const data: any = {};
   (Object.keys(mercury.db) as (keyof DB)[]).forEach((model: keyof DB) => {
     data[model] = t.router({
-      get: genProcedure(model),
+      get: genProcedure(model, "get"),
+      list: genProcedure(model, "list"),
+      paginate: genProcedure(model, "paginate"),
+      count: genProcedure(model, "count"),
+      create: genMutationProcedure(model, "create"),
+      update: genMutationProcedure(model, "update"),
+      delete: genMutationProcedure(model, "delete")
     });
   });
   createInterfaces(mercury); // This will generate the type defs file
