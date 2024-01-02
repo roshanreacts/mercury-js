cec7e21271d091454b6f22076143f937d405be14
Roshan K Gujarathi <roshankumargujarathi@gmail.com>
2024-01-02 17:05:49 +0530

    v beta 11

--- packages/core/src/utility.ts
+++ packages/core/src/utility.ts
@@ -160,7 +160,7 @@ input whereString {
   notStartWith: String
   endsWith: String
   notEndsWith: String
-  isIn: [String]
+  in: [String]
   notIn: [String]
 }
 
@@ -228,46 +228,46 @@ export const whereInputMap = (input: any, modelFields: TFields) => {
         querySchema._id = _.has(fieldReq, 'is')
           ? { $eq: fieldReq.is }
           : _.has(fieldReq, 'isNot')
-          ? { $ne: fieldReq.isNot }
-          : _.has(fieldReq, 'in')
-          ? { $in: fieldReq.in }
-          : _.has(fieldReq, 'notIn')
-          ? { $nin: fieldReq.notIn }
-          : null;
+            ? { $ne: fieldReq.isNot }
+            : _.has(fieldReq, 'in')
+              ? { $in: fieldReq.in }
+              : _.has(fieldReq, 'notIn')
+                ? { $nin: fieldReq.notIn }
+                : null;
         break;
       case 'relationship':
         querySchema[field] = _.has(fieldReq, 'is')
           ? { $eq: fieldReq.is }
           : _.has(fieldReq, 'isNot')
-          ? { $ne: fieldReq.isNot }
-          : _.has(fieldReq, 'in')
-          ? { $in: fieldReq.in }
-          : _.has(fieldReq, 'notIn')
-          ? { $nin: fieldReq.notIn }
-          : null;
+            ? { $ne: fieldReq.isNot }
+            : _.has(fieldReq, 'in')
+              ? { $in: fieldReq.in }
+              : _.has(fieldReq, 'notIn')
+                ? { $nin: fieldReq.notIn }
+                : null;
         break;
       case 'String':
         querySchema[field] = _.has(fieldReq, 'is')
           ? { $eq: fieldReq.is }
           : _.has(fieldReq, 'isNot')
-          ? { $ne: fieldReq.isNot }
-          : _.has(fieldReq, 'contains')
-          ? { $regex: `${fieldReq.contains}`, $options: 'i' }
-          : _.has(fieldReq, 'notContains')
-          ? { $regex: `^((?!${fieldReq.notContains}).)*$`, $options: 'i' }
-          : _.has(fieldReq, 'startsWith')
-          ? { $regex: `^${fieldReq.startsWith}`, $options: 'i' }
-          : _.has(fieldReq, 'notStartWith')
-          ? { $not: { $regex: `^${fieldReq.notStartWith}.*`, $options: 'i' } }
-          : _.has(fieldReq, 'endsWith')
-          ? { $regex: `.*${fieldReq.endsWith}$`, $options: 'i' }
-          : _.has(fieldReq, 'notEndsWith')
-          ? { $not: { $regex: `.*${fieldReq.notEndsWith}$`, $options: 'i' } }
-          : _.has(fieldReq, 'in')
-          ? { $in: fieldReq.in }
-          : _.has(fieldReq, 'notIn')
-          ? { $nin: fieldReq.notIn }
-          : null;
+            ? { $ne: fieldReq.isNot }
+            : _.has(fieldReq, 'contains')
+              ? { $regex: `${fieldReq.contains}`, $options: 'i' }
+              : _.has(fieldReq, 'notContains')
+                ? { $regex: `^((?!${fieldReq.notContains}).)*$`, $options: 'i' }
+                : _.has(fieldReq, 'startsWith')
+                  ? { $regex: `^${fieldReq.startsWith}`, $options: 'i' }
+                  : _.has(fieldReq, 'notStartWith')
+                    ? { $not: { $regex: `^${fieldReq.notStartWith}.*`, $options: 'i' } }
+                    : _.has(fieldReq, 'endsWith')
+                      ? { $regex: `.*${fieldReq.endsWith}$`, $options: 'i' }
+                      : _.has(fieldReq, 'notEndsWith')
+                        ? { $not: { $regex: `.*${fieldReq.notEndsWith}$`, $options: 'i' } }
+                        : _.has(fieldReq, 'in')
+                          ? { $in: fieldReq.in }
+                          : _.has(fieldReq, 'notIn')
+                            ? { $nin: fieldReq.notIn }
+                            : null;
         break;
       case 'enum':
         querySchema[field] = { $eq: fieldReq };
@@ -280,20 +280,20 @@ export const whereInputMap = (input: any, modelFields: TFields) => {
         querySchema[field] = _.has(fieldReq, 'is')
           ? { $eq: fieldReq.is }
           : _.has(fieldReq, 'isNot')
-          ? { $ne: fieldReq.isNot }
-          : _.has(fieldReq, 'lt')
-          ? { $lt: fieldReq.lt }
-          : _.has(fieldReq, 'lte')
-          ? { $lte: fieldReq.lte }
-          : _.has(fieldReq, 'gt')
-          ? { $gt: fieldReq.gt }
-          : _.has(fieldReq, 'gte')
-          ? { $gte: fieldReq.gte }
-          : _.has(fieldReq, 'in')
-          ? { $in: fieldReq.in }
-          : _.has(fieldReq, 'notIn')
-          ? { $nin: fieldReq.notIn }
-          : null;
+            ? { $ne: fieldReq.isNot }
+            : _.has(fieldReq, 'lt')
+              ? { $lt: fieldReq.lt }
+              : _.has(fieldReq, 'lte')
+                ? { $lte: fieldReq.lte }
+                : _.has(fieldReq, 'gt')
+                  ? { $gt: fieldReq.gt }
+                  : _.has(fieldReq, 'gte')
+                    ? { $gte: fieldReq.gte }
+                    : _.has(fieldReq, 'in')
+                      ? { $in: fieldReq.in }
+                      : _.has(fieldReq, 'notIn')
+                        ? { $nin: fieldReq.notIn }
+                        : null;
         break;
       default:
         break;
