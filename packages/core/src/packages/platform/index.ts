67907c685ebb0cc7ba0e11503ae253d0a453389e
Roshan K Gujarathi <roshankumargujarathi@gmail.com>
2024-01-04 13:55:00 +0530

    Add initialization and schema generation methods to Platform class

--- packages/core/src/packages/platform/index.ts
+++ packages/core/src/packages/platform/index.ts
@@ -13,6 +13,7 @@ declare module '../../mercury' {
 export default (config?: PlatformConfig) => {
     return (mercury: Mercury) => {
         mercury.platform = new Platform(mercury, config);
+        // initialize here
     };
 };
 
@@ -23,4 +24,18 @@ export class Platform {
         this.mercury = mercury;
         this.config = config || {};
     }
+    public start() {
+        // Get all the models from Model table and generate model schema and pass it to mercury.createModel
+        // Store all the model schemas as Redis Cache with search capability (Make sure redis is enabled)
+        // 
+    }
+    public initialise() {
+        // Create ModelMetadata, Model, FieldAttributes, ModelOptions using mercury.createModel
+        // Create index in Model (ModelMetada.Id, FieldName)
+    }
+    // methods:
+    // listModels (Redis cache)
+    // getModel (Redis cache)
+    // create/update/deleteModel (store the schema to DB and update the redis cache)
+
 }
