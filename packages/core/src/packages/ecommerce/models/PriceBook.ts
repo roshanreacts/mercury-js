cfb437c49729b9b989f4a01d1896bc54126d113d
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2024-08-05 14:50:54 +0530

    made price book items virual in price book

--- packages/core/src/packages/ecommerce/models/PriceBook.ts
+++ packages/core/src/packages/ecommerce/models/PriceBook.ts
@@ -1,26 +1,29 @@
-export const PriceBook:PModel={
-    info: {
-        name: 'PriceBook',
-        label: 'PriceBook',
-        description: 'PriceBook model',
-        managed: true,
-        prefix: 'PRICEBOOK'
-      },
-      fields: {
-       currency : {
-        type : "float"
-       },
-       name : {
-        type : "string"
-       },
-       priceBookItem : {
-        type : "relationship",
-        ref : "PriceBookItem"
-       },
-      
-      },
-      options: {
-       historyTracking: false
-      }
-      
+export const PriceBook: PModel = {
+  info: {
+    name: 'PriceBook',
+    label: 'PriceBook',
+    description: 'PriceBook model',
+    managed: true,
+    prefix: 'PRICEBOOK'
+  },
+  fields: {
+    currency: {
+      type: "float"
+    },
+    name: {
+      type: "string"
+    },
+    priceBookItem: {
+      type: "virtual",
+      ref: "PriceBookItem",
+      localField: "_id",
+      foreignField: "priceBook",
+      many: true,
+    },
+
+  },
+  options: {
+    historyTracking: false
+  }
+
 }
