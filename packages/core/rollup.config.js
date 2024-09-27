5a9d82078e3461db5aebf2fe8c9eb777e393df65
PrashanthBeri <prashanthberi00@gmail.com>
2024-09-27 12:22:46 +0530

    fixed rollup error

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,17 +1,15 @@
-const { withNx } = require('@nx/rollup/with-nx');
+import { withNx } from '@nx/rollup/with-nx.js';
 
-module.exports = withNx(
+export default withNx(
   {
     main: './src/index.ts',
     outputPath: '../../dist/packages/core',
     tsConfig: './tsconfig.lib.json',
     compiler: 'tsc',
     format: ['cjs', 'esm'],
-    // assets: [{ input: '.', output: '.', glob: '*.md' }],
   },
   {
-    // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
-    // e.g.
-    // output: { sourcemap: true },
+    // Additional rollup configuration here
+    // Example: output: { sourcemap: true },
   }
 );
